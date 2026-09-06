825e8d29ba5d18b7a54a2f87785d12365d1d268e
sukhmanpreet kaur <sukhmanrandhawa2207@gmail.com>
2026-09-06 12:19:21 +0530

    second commit

--- naive_bayes.py
+++ naive_bayes.py
@@ -5,3 +5,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
 # Load the dataset
+df = pd.read_csv('C:\\Users\\sukhm\\OneDrive\\Desktop\\pydev\\sentiment analysis\\IMDB_Dataset.csv')
+# print(df.head(3))
+df['sentiment'] = df['sentiment'].replace({'positive': 1, 'negative': 0})
+print(df.head(3))
